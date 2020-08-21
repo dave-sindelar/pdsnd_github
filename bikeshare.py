235a39b33d6aa0b520c98639c5ea89936b30b6b9
David L. Sindelar <ignatz2@cox.net>
2020-08-21 09:48:24 -0500

    Docs: added comment to main function

--- bikeshare.py
+++ bikeshare.py
@@ -257,6 +257,7 @@ def get_five(df, point_start, city):
     return pointerval + 1
 
 def main():
+    # central module hub
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
